86aa845958c43ce44f3bf06ecf46c271d01d1a90
Viltonedson <figueiroaedson@hotmail.com>
2024-09-19 23:35:35 -0300

    "app.js"

---
+++ models/user.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+
+//Criando entidade(tabela)
+const User = mongoose.model('User',{
+    name: String,
+    email: String,
+    password: String,
+    numerotelefone: String,
+    datanascimento: Date,
+    cpf: String
+})
+
+module.exports = User
