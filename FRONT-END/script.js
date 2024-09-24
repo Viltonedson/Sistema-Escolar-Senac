1efaaadb4fac936c5123331c35dcd4fb7a37ef53
evaldo.venceslau@hotmail.com <evaldo.venceslau@hotmail.com>
2024-09-24 20:57:16 -0300

    "front-end evaldo"

--- FRONT-END/script.js
+++ FRONT-END/script.js
@@ -1,27 +1,12 @@
-
-var btnSignin = document.querySelector("#signin");
-var btnSignup = document.querySelector("#signup");
-
-var body = document.querySelector("body");
-
-
-btnSignin.addEventListener("click", function () {
-   body.className = "sign-in-js"; 
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    
+    if (email && password) {
+        alert('Login realizado com sucesso!');
+    } else {
+        alert('Por favor, preencha todos os campos.');
+    }
 });
-
-btnSignup.addEventListener("click", function () {
-    body.className = "sign-up-js";
-})
-
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('service-worker.js')
-            .then(registration => {
-                console.log('Service Worker registrado com sucesso:', registration);
-            })
-            .catch(error => {
-                console.log('Falha ao registrar o Service Worker:', error);
-            });
-    });
-}
-
